ebcc4cd52c97ff9862b3f043e277828cbf502299
Polina.Shevchenko <polina_shevchenko_2005@list.ru>
2025-01-17 19:05:46 +0300

    Update test_hello.py

--- test_hello.py
+++ test_hello.py
@@ -2,7 +2,7 @@ import unittest
 from hello import say_hello
 
 class Tests_say_hello(unittest.TestCase):
-    def test_say_hello(self):
+    def test_say_hello(self): 
         self.assertEqual(say_hello("Julia"), "Hello, Julia!")
         self.assertEqual(say_hello("Anastasia"), "Hello, Anastasia!")
         self.assertEqual(say_hello(""), "Hello, !")
