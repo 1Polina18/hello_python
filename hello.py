1ca7d62a1ff875cd2f8d6f7bab47e8468b166c60
Polina Shevchenko <polina_shevchenko_2005@list.ru>
2025-01-17 18:57:10 +0300

    Initial commit with hello_python script 2

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
 def say_hello(name: str) -> str:
     return f"Hello, {name}!"
 
+
 if __name__ == "__main__":
     user_name = input("Enter your name: ")
     print(say_hello(user_name))
---
+++ test_hello.py
@@ -0,0 +1,11 @@
+import unittest
+from hello import say_hello
+
+class Tests_say_hello(unittest.TestCase):
+    def test_say_hello(self):
+        self.assertEqual(say_hello("Julia"), "Hello, Julia!")
+        self.assertEqual(say_hello("Anastasia"), "Hello, Anastasia!")
+        self.assertEqual(say_hello(""), "Hello, !")
+
+if __name__ == "__main__":
+    unittest.main()
