5c44203a914fda03ee32659492c78a7a9f6f16cb
Polina.Shevchenko <polina_shevchenko_2005@list.ru>
2025-05-25 20:31:07 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,5 +3,5 @@ def say_hello(name: str) -> str:
 
 
 if __name__ == "__main__":
-    user_name = input("En ter your name: ")
+    user_name = input("En1 ter your name: ")
     print(say_hello(user_name))
